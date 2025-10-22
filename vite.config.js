06ee9f2799174b0c418425400031e434146db220
claudiaacchurch <claudiachurch00@gmail.com>
2025-10-22 17:28:37 +0100

    update base & index.html

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-	base: "/lucy-laura-scott/",
-	build: { outDir: 'dist' },
+export default defineConfig(({ command }) => ({
+	base: command === "build" ? "/lucy-laura-scott/" : "/",
+	build: { outDir: "dist" },
 	plugins: [react()],
-});
+}));
